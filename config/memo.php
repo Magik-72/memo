2bcd83fc47956e0ad96419736af55581464c3d46
guillaume <guillaume.rival@gmail.com>
2021-11-17 10:58:22 +0100

    delete config values

--- config/memo.php
+++ config/memo.php
@@ -1,14 +1,3 @@
 <?php return [
-    'size' => 78787,
-    'hello' => 'world',
-    'attributes' => [
-        'category_a' => [
-            'size' => 5,
-            'count' => 12
-        ],
-        'image' => [
-            'size' => 5,
-            'count' => 12,
-        ]
-    ]
+
 ];
